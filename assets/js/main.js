3fc4e97a44786adcfeb15be71b90c83c02b0864f
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-03 14:51:16 -0400

    Haciendo visibles las imagenes para cda animal

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,36 +14,51 @@ from './datos.js'
 const animales = [];
 let img = document.getElementById('preview')
 let imagen = document.createElement('img')
+/* let imagen = document.createElement('img')
+let imagen = document.createElement('img')
+let imagen = document.createElement('img') */
 const registrar = document.getElementById('btnRegistrar');
 registrar.addEventListener('click', async () => {
     const nombre = document.getElementById('animal');
     const edad = document.getElementById('edad');
     const comentario = document.getElementById('edad')
-
     const datos = await mandarDatos.mostrar;
-    console.log(datos.animales[4].imagen)
-    /*  const leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
+    const leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
     const lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
     const oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
     const serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
-   */
     const aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
     img.appendChild(imagen);
-    const imagenAguila = aguila.img;
-    imagen.setAttribute('src', `assets/imgs/${imagenAguila}`)
-    imagen.setAttribute('width', '80%')
-    imagen.setAttribute('alt','80%')
+    let imagenAguila = aguila.img;
+    let imagenLeon = leon.img;
+    let imagenLobo = lobo.img;
+    let imagenOso = oso.img;
+    let imagenSerpiente = serpiente.img;
+
+    if (!imagenAguila) {
+        mostrarImagen(imagenAguila)       
+    } else if (!imagenLeon){
+        mostrarImagen(imagenLeon)     
+    }else if(!imagenLobo){
+        mostrarImagen(imagenLobo)
+    
+    }else if(!imagenSerpiente){   
+        mostrarImagen(imagenSerpiente)
+    }else if(!imagenOso){
+        mostrarImagen(imagenOso)
+    }
+
 
 
 });
 
 
-/* 
-const mostrarImagen = (imagen) => {
-    if (imagen != null) {
-        imagen.removeChild;
-    } else {
-        imagen.setAttribute('src', `assets/imgs/${imagen}`)
-        imagen.setAttribute('width', '80%')
-    }
-} */
+//mostrar imagen
+const mostrarImagen = (imagenAnimal) => {
+
+    imagen.setAttribute('src', `assets/imgs/${imagenAnimal}`)
+    imagen.setAttribute('width', '80%')
+    imagen.setAttribute('alt', '80%')
+    /*  */
+
+}
