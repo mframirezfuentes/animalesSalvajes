e48df3eae836595b6bafa7d6846735b538f2b4c9
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-04 11:04:11 -0400

    Validacion de los campos

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,35 +21,50 @@ const registrar = document.getElementById('btnRegistrar');
 registrar.addEventListener('click', async () => {
     const nombre = document.getElementById('animal');
     const edad = document.getElementById('edad');
-    const comentario = document.getElementById('edad')
-    const datos = await mandarDatos.mostrar;
-    const leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
-    const lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
-    const oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
-    const serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
-    const aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
-    img.appendChild(imagen);
-    let imagenAguila = aguila.img;
-    let imagenLeon = leon.img;
-    let imagenLobo = lobo.img;
-    let imagenOso = oso.img;
-    let imagenSerpiente = serpiente.img;
+    const comentario = document.getElementById('comentarios')
 
-    if (nombre.value== 'Aguila') {
-        mostrarImagen(imagenAguila)       
-    } else if (nombre.value== 'Leon'){
-        mostrarImagen(imagenLeon)     
-    }else if(nombre.value== 'Lobo'){
-        mostrarImagen(imagenLobo)
     
-    }else if(nombre.value== 'Serpiente'){   
-        mostrarImagen(imagenSerpiente)
-    }else if(nombre.value== 'Oso'){
-        mostrarImagen(imagenOso)
-    }
+        if (nombre.value && edad.value && comentario.value) {
+            const datos = await mandarDatos.mostrar;
+            const leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
+            const lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
+            const oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
+            const serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
+            const aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
+            img.appendChild(imagen);
+
+            let imagenLeon = leon.img;
+            let imagenLobo = lobo.img;
+            let imagenOso = oso.img;
+            let imagenSerpiente = serpiente.img;
+
+            if (nombre.value == 'Aguila' && edad.value != null && comentario.value != null) {
+                let imagenAguila = aguila.img;
+                console.log('Estoy pasando por aquí')
+                mostrarImagen(imagenAguila)
+            } else if (nombre.value == 'Leon' && edad.value != null && comentario.value != null) {
+                mostrarImagen(imagenLeon)
+            } else if (nombre.value == 'Lobo' && edad.value != null && comentario.value != null) {
+                mostrarImagen(imagenLobo)
+            } else if (nombre.value == 'Serpiente' && edad.value != null && comentario.value != null) {
+                mostrarImagen(imagenSerpiente)
+                imagen.setAttribute('width', '110%')
+                imagen.setAttribute('alt', 'auto')
+            } else if (nombre.value == 'Oso' && edad.value != null && comentario.value != null) {
+                mostrarImagen(imagenOso)
+                imagen.setAttribute('width', '110%')
+                imagen.setAttribute('alt', 'auto')
+            }
+         
+        } else{
+            alert('Tiene que ingresar todos los datos')
+        }
+    
 
 
+       
 
+    
 });
 
 
@@ -57,7 +72,7 @@ registrar.addEventListener('click', async () => {
 const mostrarImagen = (imagenAnimal) => {
 
     imagen.setAttribute('src', `assets/imgs/${imagenAnimal}`)
-    imagen.setAttribute('width', '90%')
+    imagen.setAttribute('width', '87%')
     imagen.setAttribute('alt', '100%')
     /*  */
 
