45e0b09f95f982661715339b6bcced331665802b
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-04 11:32:31 -0400

    Validacion de los campos, y creacion de un animal dependiendo cual se escoja

--- assets/js/datos.js
+++ assets/js/datos.js
@@ -1,19 +1,9 @@
-
-
 export const mandarDatos=(()=>{
-
     const obtenerDatos = async () => {
         const url = 'http://127.0.0.1:5500/animales.json';
         const animales = await fetch(url);
         const respuesta = await animales.json();    
         return respuesta;    
     }
-
-
-
-    return{
-        mostrar:     obtenerDatos()
-            }
-
-
+    return{mostrar:obtenerDatos()}
 })();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-import {
-    Animal
-} from './animal.js'
+
 import Aguila from './aguila.js'
 import Leon from './leon.js'
 import Lobo from './lobo.js'
@@ -10,7 +8,6 @@ import {
     mandarDatos
 }
 from './datos.js'
-
 const animales = [];
 let img = document.getElementById('preview')
 let imagen = document.createElement('img')
@@ -22,58 +19,41 @@ registrar.addEventListener('click', async () => {
     const nombre = document.getElementById('animal');
     const edad = document.getElementById('edad');
     const comentario = document.getElementById('comentarios')
-
-    
-        if (nombre.value && edad.value && comentario.value) {
-            const datos = await mandarDatos.mostrar;
-            const leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
-            const lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
-            const oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
-            const serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
-            const aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
-            img.appendChild(imagen);
-
+    if (nombre.value && edad.value && comentario.value) {
+        let datos = await mandarDatos.mostrar;
+        if (nombre.value == 'Leon') {
+            let leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
             let imagenLeon = leon.img;
+            mostrarImagen(imagenLeon)
+        } else if (nombre.value == 'Lobo') {
+            let lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
             let imagenLobo = lobo.img;
+            mostrarImagen(imagenLobo)
+        } else if (nombre.value == 'Oso') {
+            let oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
             let imagenOso = oso.img;
+            mostrarImagen(imagenOso)
+            imagen.setAttribute('width', '110%')
+            imagen.setAttribute('alt', 'auto')
+        } else if (nombre.value == 'Serpiente') {
+            let serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
             let imagenSerpiente = serpiente.img;
-
-            if (nombre.value == 'Aguila' && edad.value != null && comentario.value != null) {
-                let imagenAguila = aguila.img;
-                console.log('Estoy pasando por aquí')
-                mostrarImagen(imagenAguila)
-            } else if (nombre.value == 'Leon' && edad.value != null && comentario.value != null) {
-                mostrarImagen(imagenLeon)
-            } else if (nombre.value == 'Lobo' && edad.value != null && comentario.value != null) {
-                mostrarImagen(imagenLobo)
-            } else if (nombre.value == 'Serpiente' && edad.value != null && comentario.value != null) {
-                mostrarImagen(imagenSerpiente)
-                imagen.setAttribute('width', '110%')
-                imagen.setAttribute('alt', 'auto')
-            } else if (nombre.value == 'Oso' && edad.value != null && comentario.value != null) {
-                mostrarImagen(imagenOso)
-                imagen.setAttribute('width', '110%')
-                imagen.setAttribute('alt', 'auto')
-            }
-         
-        } else{
-            alert('Tiene que ingresar todos los datos')
+            mostrarImagen(imagenSerpiente)
+            imagen.setAttribute('width', '110%')
+            imagen.setAttribute('alt', 'auto')
+        } else if (nombre.value == 'Aguila') {
+            let aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
+            let imagenAguila = aguila.img;
+            mostrarImagen(imagenAguila)
         }
-    
-
-
-       
-
-    
+        img.appendChild(imagen);
+    } else {
+        alert('Tiene que ingresar todos los datos')
+    }
 });
-
-
 //mostrar imagen
 const mostrarImagen = (imagenAnimal) => {
-
     imagen.setAttribute('src', `assets/imgs/${imagenAnimal}`)
     imagen.setAttribute('width', '87%')
     imagen.setAttribute('alt', '100%')
-    /*  */
-
 }
