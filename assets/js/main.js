4d6cd64b7605a1448fd287106f0206c25308979a
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-06-04 12:26:14 -0400

    se agregan al arrays los animales que se van agregando, falta borrar el campo después que se agregan

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,40 +16,57 @@ let imagen = document.createElement('img')
 let imagen = document.createElement('img') */
 const registrar = document.getElementById('btnRegistrar');
 registrar.addEventListener('click', async () => {
-    const nombre = document.getElementById('animal');
-    const edad = document.getElementById('edad');
-    const comentario = document.getElementById('comentarios')
-    if (nombre.value && edad.value && comentario.value) {
+    let nombre = document.getElementById('animal');
+    let edad = document.getElementById('edad');
+    let comentario = document.getElementById('comentarios')
+    if (nombre && edad && comentario) {
         let datos = await mandarDatos.mostrar;
         if (nombre.value == 'Leon') {
             let leon = new Leon(nombre.value, edad.value, datos.animales[0].imagen, comentario.value, datos.animales[0].sonido);
             let imagenLeon = leon.img;
-            mostrarImagen(imagenLeon)
+            mostrarImagen(imagenLeon);
+            console.log(leon);
+            animales.push(leon);
         } else if (nombre.value == 'Lobo') {
             let lobo = new Lobo(nombre.value, edad.value, datos.animales[1].imagen, comentario.value, datos.animales[1].sonido);
             let imagenLobo = lobo.img;
-            mostrarImagen(imagenLobo)
+            mostrarImagen(imagenLobo);
+            console.log(lobo);
+            animales.push(lobo);
         } else if (nombre.value == 'Oso') {
             let oso = new Oso(nombre.value, edad.value, datos.animales[2].imagen, comentario.value, datos.animales[1].sonido);
             let imagenOso = oso.img;
-            mostrarImagen(imagenOso)
-            imagen.setAttribute('width', '110%')
-            imagen.setAttribute('alt', 'auto')
+            mostrarImagen(imagenOso);
+            imagen.setAttribute('width', '110%');
+            imagen.setAttribute('alt', 'auto');
+            console.log(oso);
+            animales.push(oso);
         } else if (nombre.value == 'Serpiente') {
             let serpiente = new Serpiente(nombre.value, edad.value, datos.animales[3].imagen, comentario.value, datos.animales[1].sonido);
             let imagenSerpiente = serpiente.img;
-            mostrarImagen(imagenSerpiente)
-            imagen.setAttribute('width', '110%')
-            imagen.setAttribute('alt', 'auto')
+            mostrarImagen(imagenSerpiente);
+            imagen.setAttribute('width', '110%');
+            imagen.setAttribute('alt', 'auto');
+            console.log(serpiente);
+            animales.push(serpiente)
         } else if (nombre.value == 'Aguila') {
             let aguila = new Aguila(nombre.value, edad.value, datos.animales[4].imagen, comentario.value, datos.animales[4].sonido)
             let imagenAguila = aguila.img;
-            mostrarImagen(imagenAguila)
+            mostrarImagen(imagenAguila);
+            console.log(aguila);
+            animales.push(aguila);
         }
+        
         img.appendChild(imagen);
+        nombre='';
+        edad='';
+        comentario=''
+        console.log(animales)
+     
     } else {
-        alert('Tiene que ingresar todos los datos')
+        alert('Tiene que ingresar todos los datos');
     }
+
 });
 //mostrar imagen
 const mostrarImagen = (imagenAnimal) => {
